f0a267704de689b83b70e79cd89ac8057130a2e2
marialungu <anauna.lungu@gmail.com>
2021-12-19 12:57:46 +0200

    refactor

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 import {registerRestaurantContainer} from './src/components/restaurant-card.js'
 import './main.scss'
-import AlgoliaSearch from "./src/js/search";
 
 // new AlgoliaSearch().executeSearch();
-registerRestaurantContainer().render()
-
-alert('connected')
+registerRestaurantContainer()
