f667c4ab201eca5087c468f675b6948354939560
marialungu <anauna.lungu@gmail.com>
2021-12-21 19:07:34 +0200

    add responsive styles for phone

--- src/components/render-no-results.js
+++ src/components/render-no-results.js
@@ -2,7 +2,7 @@ export const renderNoResults = () => {
     const noResults = document.createElement('div')
     noResults.setAttribute('class', 'no-results')
     noResults.innerHTML = `
-        <img src="./images/icn-no-results.png">
+        <img class="no-results-img" src="./images/icn-no-results.png">
         <div class="no-results-text">Ooops... looks like there were no restaurants found</div>
     `
     const elementsContainer = document.querySelector('#container');
